708df038a82a25fc84fb5d286ddccc5ff4a9c002
LisaTr <trlis369@gmail.com>
2020-10-31 14:51:32 -0400

    Halloween Project created

--- src/main/java/HalloweenDecorations.java
+++ src/main/java/HalloweenDecorations.java
@@ -2,6 +2,7 @@ public class HalloweenDecorations {
 
     String decorSkeletons = "Skeletons on the street";
     String decorPumpkins = "Pumpkins are craved";
+    String decorWebs = "Houses covered in spider webs";
 
 
 }
--- src/test/java/HalloweenDecorationsTest.java
+++ src/test/java/HalloweenDecorationsTest.java
@@ -4,6 +4,7 @@ public class HalloweenDecorationsTest {
 
         System.out.println("There are skeletons on the street");
         System.out.println("There are pumpkins craved on the at the doors");
+        System.out.println("Houses sre covered in spider webs");
 
     }
 
